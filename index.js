7c4f595e50013e3d5c10e4b7e83526cd9ea1b416
zdflower <zdflower@users.noreply.github.com>
2017-07-08 11:05:24 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,9 @@
-        //reescribir esto con menos redundancia
-    $("#navContact").on("click", function(){
-       $(this).addClass('active'); 
-        $("#navPortfolio").removeClass('active');
-        $("#navAbout").removeClass('active');
-    });
-        
-        $("#navAbout").on("click", function(){
-       $(this).addClass('active'); 
-        $("#navPortfolio").removeClass('active');
-        $("#navContact").removeClass('active');
-    });
-        
-    $("#navPortfolio").on("click", function(){
-       $(this).addClass('active'); 
-        $("#navAbout").removeClass('active');
-        $("#navContact").removeClass('active');
-    });
-
+'use strict';
+$(document).ready(function () {
+        var listaNav = $(".top-nav"); //selecciono la lista de navegación
+	var botonesNav = listaNav.find('li'); //selecciono los botones de la lista de navegación (y no los a dentro de li, ¿funcionaría si seleccionara li a? ¿tendría siblings un li a?)
+	botonesNav.on('click', function () { //les agrego un evento de click
+                $(this).addClass('active'); //al botón clickeado le agrego la clase active
+                $(this).siblings().removeClass('active'); // a los demás se la saco.
+        });
+});
